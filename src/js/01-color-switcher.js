5cb350b1b04cdcfec9d63bdf142bb8ea6542afa7
Sergii Marchenko <marikmarchelo@gmail.com>
2023-06-06 21:26:03 +0300

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const colorBody = document.querySelector('body');
+let timerId = null;
 
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart(event) {
+  event.preventDefault();
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    colorBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStop(event) {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
