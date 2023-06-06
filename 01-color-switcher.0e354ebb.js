5c6e45a95485605a08f0359ef4692751f04c9c26
SergiiMarch <111069200+SergiiMarch@users.noreply.github.com>
2023-06-06 18:26:25 +0000

    Deploying to gh-pages from @ SergiiMarch/goit-js-hw-09@5cb350b1b04cdcfec9d63bdf142bb8ea6542afa7 🚀

---
+++ 01-color-switcher.0e354ebb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(a){a.preventDefault(),t.disabled=!0,e.disabled=!1,o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(n){t.disabled=!1,e.disabled=!0,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.0e354ebb.js.map
