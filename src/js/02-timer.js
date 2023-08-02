60618b600da669d3ad979d0a57a4b13a7e307d6a
Sergii Marchenko <marikmarchelo@gmail.com>
2023-08-02 20:58:50 +0300

    add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,13 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const dataTime = document.getElementById('datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 const dataDays = document.querySelector('span[data-days]');
 const dataHours = document.querySelector('span[data-hours]');
 const dataMinutes = document.querySelector('span[data-minutes]');
 const dataSeconds = document.querySelector('span[data-seconds]');
-
+const dataTime = document.querySelector('input#datetime-picker');
 let timerId = null;
 startBtn.disabled = true;
 
@@ -19,7 +19,7 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
       startBtn.disabled = true;
-      alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
     } else {
       startBtn.disabled = false;
     }
@@ -28,21 +28,29 @@ const options = {
 
 flatpickr(dataTime, options);
 
-startBtn.addEventListener('click', onClick);
+startBtn.addEventListener('click', onBtnStart);
 
-function onClick(evt) {
+function onBtnStart(event) {
   timerId = setInterval(() => {
-    let timeDate = new Date(dataTime.value) - Date.now();
-    let countDownTime = convertMs(timeDate);
-    console.log(
-      '🚀 ~ file: 02-timer.js:38 ~ setInterval ~ countDownTime:',
-      countDownTime
-    );
-    dataDays.textContent = padValue(countDownTime.days);
-    dataHours.textContent = padValue(countDownTime.hours);
-    dataMinutes.textContent = padValue(countDownTime.minutes);
-    dataSeconds.textContent = padValue(countDownTime.seconds);
+    let countTime = new Date(dataTime.value) - Date.now();
+    let timeObject = convertMs(countTime);
+    if (countTime > 0) {
+      dataDays.textContent = addLeadingZero(timeObject.days);
+      dataHours.textContent = addLeadingZero(timeObject.hours);
+      dataMinutes.textContent = addLeadingZero(timeObject.minutes);
+      dataSeconds.textContent = addLeadingZero(timeObject.seconds);
+    } else if (countTime <= 0) {
+      clearInterval(timerId);
+      Notiflix.Notify.success('Congratulation!');
+      startBtn.disabled = true;
+    }
   }, 1000);
+}
+
+/-----------------------------------------------------/;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
 }
 
 function convertMs(ms) {
@@ -63,26 +71,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-//функція добавляє другій елемнт до таймера,в нашому віпадку 0 (01 04 2023)
-function padValue(value) {
-  return String(value).padStart(2, 0);
-}
-
-// const timer = {
-//   start() {
-//     const startDate = Date.now();
-
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startDate;
-//       const { days, hours, minutes, seconds } = convertMs(deltaTime);
-//       console.log(`${days} : ${hours} : ${minutes} : ${seconds}`);
-//     }, 8000);
-//   },
-// };
-// timer.start();
