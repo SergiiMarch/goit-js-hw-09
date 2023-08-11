82c5f125020e7e5c81a251f3fc5986e40626c15c
SergiiMarch <111069200+SergiiMarch@users.noreply.github.com>
2023-08-11 05:56:17 +0000

    Deploying to gh-pages from @ SergiiMarch/goit-js-hw-09@b4fb3591b8c692ad92ec9f737e890c53b07481ec 🚀

---
+++ 03-promises.2b18ccfa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");console.dir(e),e.addEventListener("submit",(function(l){l.preventDefault();e.elements.delay.value,e.elements.dalay.value,e.elements.dalay.value}));
+//# sourceMappingURL=03-promises.2b18ccfa.js.map
